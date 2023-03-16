146cb143f4e0111360feb7f106dc9efc1ffeb8fb
hi-lary <beansy7@sbcglobal.net>
2023-03-16 15:01:23 -0500

    homework

--- fundamentals_constants.py
+++ fundamentals_constants.py
@@ -1,12 +1,15 @@
 # exercise 6.1
+#using constants.txt
 
 def read_data(filename):
-    infile = open(filename, 'r').readlines()[2:-1]
-    constants = {}
+    infile = open(filename, 'r').readlines()[2:-1]  # we open the file and are in 'r' mode (read mode).
+    # we use readlines to start at line 2 and go to the end of the file
+    constants = {}  # opens a dictionary that we will add stuff to below
     for line in infile:
-        words = line.split()
-        constant = float(words[-2])
-        if len(words[:-2]) == 3:
+        words = line.split()  # creates a word after every space
+        constant = float(words[-2])  # where our number is in the constants.txt file
+        if len(words[:-2]) == 3:  # some constants have 3 words and some have 2
+            # we combine them into one word in the next couple lines
             object = words[0] + ' ' + words[1] + ' ' + words[2]
         elif len(words[:-2]) == 2:
             object = words[0] + ' ' + words[1]
--- read_error.py
+++ read_error.py
@@ -1,1 +1,21 @@
 # exercise 6.4
+# using lnsum.txt
+import numpy as np
+
+
+def read_data(filename):
+    infile = open(filename, 'r').readlines()[39:44]  # only care about lines 40-44 in the txt file
+    # making empty arrays for the data from lnsum.txt
+    epsilon_array = np.array([])
+    error_array = np.array([])
+    n_array = np.array([])
+    for line in infile:
+        words = line.split()
+        epsilon = float(words[1][0]) * 10 ** float(words[1][-4:-1])  # really dumb way to work around scientific notation in the txt file
+        epsilon_array = np.append(epsilon_array, epsilon)  # appends the values of epsilon to its empty array
+        error = float(words[4][3]) * 10 ** float(words[4][-4:-1])
+        error_array = np.append(error_array, error)
+    return epsilon_array, error_array, n_array
+
+
+print(read_data('lnsum.txt'))
