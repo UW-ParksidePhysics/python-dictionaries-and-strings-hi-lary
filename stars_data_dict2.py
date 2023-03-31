4d09d58e665ae9d9d6e29023c362f33afde704db
hi-lary <113123171+hi-lary@users.noreply.github.com>
2023-03-31 15:30:56 -0500

    Update stars_data_dict2.py

--- stars_data_dict2.py
+++ stars_data_dict2.py
@@ -1,1 +1,16 @@
 # exercise 6.6
+stars = {
+    'Alpha Centauri A': {'distance': 4.3, 'apparent_brightness': 0.26, 'luminosity': 1.56},
+    'Alpha Centauri B': {'distance': 4.3, 'apparent_brightness': 0.077, 'luminosity': 0.45},
+    'Alpha Centauri C': {'distance': 4.2, 'apparent_brightness': 0.00001, 'luminosity': 0.00006},
+    "Barnard's Star": {'distance': 6.0, 'apparent_brightness': 0.00004, 'luminosity': 0.0005},
+    'Wolf 359': {'distance': 7.7, 'apparent_brightness': 0.000001, 'luminosity': 0.00002},
+    'BD +36 degrees 2147': {'distance': 8.2, 'apparent_brightness': 0.0003, 'luminosity': 0.006},
+    'Luyten 726-8 A': {'distance': 8.4, 'apparent_brightness': 0.000003, 'luminosity': 0.00006},
+    'Luyten 726-8 B': {'distance': 8.4, 'apparent_brightness': 0.000002, 'luminosity': 0.00004},
+    'Sirius A': {'distance': 8.6, 'apparent_brightness': 1.00, 'luminosity': 23.6},
+    'Sirius B': {'distance': 8.6, 'apparent_brightness': 0.001, 'luminosity': 0.003},
+    'Ross 154': {'distance': 9.4, 'apparent_brightness': 0.00002, 'luminosity': 0.0005}
+  
+  distance = stars['(star name)']['distance']
+  luminosity = stars['Sirius B']['luminosity']
